f8f5739537bcca58de3a5909565fe6ccd2e81c6b
lrz <lrz0@protonmail.com>
2023-09-02 13:54:51 +0100

    entry file

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const User = require("./models/User");
+const Coinflip = require("./models/Coinflip");
+const Jackpot = require("./models/Jackpot");
+
+exports.models = {
+    User, 
+    Coinflip, 
+    Jackpot
+}
